02e55de927c779601cb5e272a423813d10186004
Lautaro Decima <decimalautaro@gmail.com>
2024-10-05 01:08:54 -0300

    feat: creating health-check controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { NatsModule } from './transports/nats.module';
 import { AuthModule } from './auth/auth.module';
+import { HealthCheckModule } from './health-check/health-check.module';
+import { HealthCheckController } from './health-check.controller';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NatsModule, AuthModule],
-  controllers: [],
+  imports: [ProductsModule, OrdersModule, NatsModule, AuthModule, HealthCheckModule],
+  controllers: [HealthCheckController],
   providers: [],
 })
 export class AppModule {}
